66093b4c7d64f3cef3b38528ccb339896b078b80
marckimbrey <marckimbrey@gmail.com>
2017-01-27 18:58:05 +0000

    Add homepage

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require('express')
+var express = require('express');
+var path = require('path');
 var app = express();
-
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/:time', function (req, res) {
   var date = new Date(req.params.time);
   var naturalDate = (date.toDateString()== 'Invalid Date')? null : date.toDateString().slice(4);
   res.end(JSON.stringify({'unix': Date.parse(date), 'natural': naturalDate}))
 });
+
+
 
 app.listen( process.env.PORT||3000)
